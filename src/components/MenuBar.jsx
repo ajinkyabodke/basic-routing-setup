6a9b260c985f3c7d55dc0fcff803293da3ef4792
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2023-11-07 21:58:42 +0530

    add styling to active route

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,14 +1,37 @@
-import { useNavigate } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 function MenuBar() {
-  const navigate = useNavigate();
+  const location = useLocation();
+
   return (
-    <div className="bg-white ">
+    <div className="bg-white">
       <h1 className="text-center text-3xl">Testing Routing</h1>
-      <div className="flex gap-5 justify-evenly text-3xl bg-slate-400 underline">
-        <button onClick={() => navigate("/")}>Home</button>
-        <button onClick={() => navigate("/about")}>About</button>
-        <button onClick={() => navigate("/contact")}>Contact</button>
+      <div className="flex gap-5 justify-evenly text-3xl bg-slate-400">
+        <NavLink
+          to="/"
+          exact
+          className={`${
+            location.pathname === "/" ? "text-blue-500" : "text-blue-200"
+          } underline`}
+        >
+          Home
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={`${
+            location.pathname === "/about" || location.pathname.startsWith("/about/") ? "text-blue-500" : "text-blue-200"
+          } underline`}
+        >
+          About
+        </NavLink>
+        <NavLink
+          to="/contact"
+          className={`${
+            location.pathname.startsWith("/contact") ? "text-blue-500" : "text-blue-200"
+          } underline`}
+        >
+          Contact
+        </NavLink>
       </div>
     </div>
   );
